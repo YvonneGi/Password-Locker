c4408ae11d3d18a802a304b40fcffb80377eaad8
YvonneGi <giryvonne@gmail.com>
2019-09-05 18:34:40 +0200

    add save_multiple method to let appn save multiple user account

--- credentials.py
+++ credentials.py
@@ -3,7 +3,7 @@ class Credentials:
     Class that generates new instances of users
     """
 
-    cred_list = []  #Empty user user_list
+    cred_list = []  # Empty user user_list
 
     def __init__(self, username, cred_app, cred_username, cred_password):
         """
@@ -12,14 +12,24 @@ class Credentials:
         self.username = username
         self.cred_app = cred_app
         self.cred_username = cred_username
-        self.cred_password = cred_password #end init cred
-   
-    ##### save method to save new object##### 
+        self.cred_password = cred_password  # end init cred
+
+    ##### save method to save new object#####
 
     def save_credentials(self):
-
         '''
         save_credentials method saves credentials objects into cred_list
         '''
 
         Credentials.cred_list.append(self)
+  ###### delete method to delete credentials when needed####
+
+    def delete_credentials(self):
+        '''
+        delete_credentials method deletes a saved credential from the cred_list
+        '''
+
+        Credentials.cred_list.remove(self)
+
+  ########search account by its credentials###
+   
--- credentials_test.py
+++ credentials_test.py
@@ -15,7 +15,15 @@ class TestCredentials(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_credential = Credentials("YvonneGi", "Twitter", "Gi", "giii")  # create user object
+        self.new_credential = Credentials(
+            "YvonneGi", "Twitter", "Gi", "giii")  # create user object
+        # end of setUp
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        Credentials.cred_list = []  # end of tearDown
 
     def test_init(self):
         '''
@@ -25,15 +33,47 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credential.username, "YvonneGi")
         self.assertEqual(self.new_credential.cred_app, "Twitter")
         self.assertEqual(self.new_credential.cred_username, "Gi")
-        self.assertEqual(self.new_credential.cred_password, "giii")
+        self.assertEqual(self.new_credential.cred_password,
+                         "giii")  # end test_init
 
     def test_save_credentials(self):
         '''
         test_save_credentials test case to test if the credential object is saved into
          the credential list
         '''
-        self.new_credential.save_credentials() # saving the new contact
-        self.assertEqual(len(Credentials.cred_list),1)
+        self.new_credential.save_credentials()  # save new credential
+        self.assertEqual(len(Credentials.cred_list), 1)  # end test_save_cred
+
+
+#  test cases for mupltiple saves here####
+
+
+    def test_save_multiple_credentials(self):
+        '''
+        test_save_multiple_credential to check if we can save multiple credential
+        objects to our cred_list
+        '''
+        self.new_credential.save_credentials()
+        test_credentials = Credentials(
+            "YvonneGi", "Twitter", "Gi", "giii")  # new credential
+        test_credentials.save_credentials()
+        self.assertEqual(len(Credentials.cred_list), 2)
+
+######### test for delete method###
+    def test_delete_credential(self):
+        '''
+        test_delete_credential to test if we can remove a credential from our credential list
+        '''
+        self.new_credential.save_credentials()
+        test_credentials = Credentials(
+            "YvonneGi", "Twitter", "Gi", "giii")  # new credential
+        test_credentials.save_credentials()
+
+        self.new_credential.delete_credentials()  # Deleting a credential object
+        self.assertEqual(len(Credentials.cred_list), 1)
+
+#######search_credential method to search for the saved credentials####
+   
 
 
 if __name__ == '__main__':
