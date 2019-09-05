83781afbddf6ea5519915fa7d4e1f3a943de8ae8
YvonneGi <giryvonne@gmail.com>
2019-09-05 13:16:35 +0200

    add save_multiple method to let appn save multiple user account

--- user_test.py
+++ user_test.py
@@ -12,10 +12,10 @@ class TestUser(unittest.TestCase):
     '''
 
     def setUp(self):
-            '''
-            Set up method to run before each test cases.
-            '''
-            self.new_user = User("YvonneGi", "woow")  # create user object
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("YvonneGi", "woow")  # create user object
 
     def test_init(self):
         '''
@@ -30,8 +30,26 @@ class TestUser(unittest.TestCase):
         test_save_user test case to test if the user object is saved into
          the user list
         '''
-        self.new_user.save_user() # saving the new contact
-        self.assertEqual(len(User.user_list),1)
+        self.new_user.save_user()  # saving the new contact
+        self.assertEqual(len(User.user_list), 1)
+  ####### use tearDown method to let app save multiple object into user list####
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+
+#  test cases for mupltiple saves here####
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user to check if we can save multiple user
+        objects to our user_list
+        '''
+        self.new_user.save_user()
+        test_user = User("YvonneGi", "woow")  # new contact
+        test_user.save_user()
+        self.assertEqual(len(User.user_list), 2)
 
 
 if __name__ == '__main__':
