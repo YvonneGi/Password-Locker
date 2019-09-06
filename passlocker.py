fda5d4be5fa5acf8052206e49232dcedda356458
YvonneGi <giryvonne@gmail.com>
2019-09-06 16:10:27 +0200

    add function to create user account into run_py

--- passlocker.py
+++ passlocker.py
@@ -4,6 +4,7 @@ from credentials import Credentials
 
    #######USER########
 
+
 def create_user(username, password):
         '''
         Function to create a new user account
@@ -11,11 +12,13 @@ def create_user(username, password):
         new_user = User(username, password)
         return new_user  # end create_user
 
+
 def save_user(user):
         '''
         Function to save user
         '''
         user.save_user()  # end save_user
+
 
 def login_user(username, password):
         '''
@@ -25,6 +28,7 @@ def login_user(username, password):
 
     #######CREDENTIALS####################
 
+
 def create_credentials(username, cred_app, cred_username, cred_password):
         '''
         Function to create a new user credentials
@@ -33,11 +37,13 @@ def create_credentials(username, cred_app, cred_username, cred_password):
             username, cred_app, cred_username, cred_password)
         return new_credential  # end create_cred
 
+
 def save_credentials(credential):
         '''
         Function to save credential
         '''
         credential.save_credential()  # end save_cred
+
 
 def delete_crededential(credential):
         '''
@@ -45,11 +51,13 @@ def delete_crededential(credential):
         '''
         credential.delete_credential()  # end delete cred
 
+
 def find_credential(username):
         '''
         Function that finds a credential by username and returns the user credential
         '''
         return Credentials.find_by_username(username)  # end find cred
+
 
 def display_credentials():
         '''
@@ -58,6 +66,7 @@ def display_credentials():
         return Credentials.display_credentials()  # end display cred
 
     ##########GENERIC################################
+
 
 def menu():
         '''
@@ -74,6 +83,7 @@ def menu():
         print('\n')
         print("             3. Exit")  # end menu
 
+
 def log_menu():
         '''
         Function to display the menu for a logged in user
@@ -85,8 +95,83 @@ def log_menu():
         print("             d. Delete Credential")
         print("             e. Logout")  # end log_menu
 
-# if __name__ == '__main__':
+# def generate_password(pass_length):
+#     '''
+#     Function to generate a random password with a custom length
+#     '''
+#     char = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
+#     gen_pass = "".join(random.choice(char) for _ in range(pass_length))
+#     return gen_pass#end generate_password
 
-#   main()
+
+############MAIN####################################
+
+
+def main():
+    menu_choice = 0
+    while menu_choice != 3:
+        menu()
+        print("\n               Your Choice: ")
+        try:
+            menu_choice = int(input("               "))
+        except:
+            print("Please Make a Valid Choice !")
+            print('Press Enter to continue')
+            input()
+
+        if menu_choice == 3:
+            print('Thanks For Using --passlocker--')
+            print('Press Enter Quit')
+            input()
+            break  # end if menu_choice = 3
+          elif menu_choice ==1:
+            print ("Create new account")
+            print("     "+"="*10)
+            print ('\n')
+            print("     Username:")
+            u_name = input()
+            print ('\n')
+            print("     Password:")
+            p_word = input()
+            save_user(create_user(u_name, p_word))  # create and save new user.
+            print ('\n')
+            print(f" New User {u_name} / Password: {p_word} created")
+            print ('Press Enter to continue')
+            input()#end if menu_choice = 1
+          elif menu_choice == 2:
+            print("     Enter Your Username and Password")
+            print("     "+"="*10)
+            print ('\n')
+            print("     Username:")
+            u_name = input()
+            print ('\n')
+            print("     Password:")
+            p_word = input()
+            logged = log_user(u_name, p_word)
+             if logged :
+                u_name = logged.username
+
+                session_header(u_name)
+
+                login_choice = ""
+                while login_choice != "e":
+                    session_header(u_name)
+
+                    log_menu()
+                    print ('\n              Your Choice: ')
+                    login_choice = input("             ")
+
+                    if login_choice == "a":
+                        session_header(u_name)
+
+                        print("       Add Credential")
+                        print("     "+"="*20)
+                        print ('\n')
+
+   
+
+if __name__ == '__main__':
+
+  main()
 
 ##################################################################################
