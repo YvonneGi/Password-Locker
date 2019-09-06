5dd6c37dbe49b0d99448e54fde41eb28a8f06680
YvonneGi <giryvonne@gmail.com>
2019-09-06 12:36:19 +0200

    add comments

--- passlocker.py
+++ passlocker.py
@@ -8,19 +8,19 @@ def create_user(username, password):
       Function to create a new user account
       '''
       new_user = User(username, password)
-      return new_user
+      return new_user#end create_user
 
 def save_user(user):
       '''
       Function to save user
       '''
-      user.save_user()
+      user.save_user()#end save_user
 
 def login_user(username, password):
       '''
       Function that finds a user by username/password and returns the user account
       '''
-      return User.login_by_userpass(username, password)
+      return User.login_by_userpass(username, password)#end login
 
 #######CREDENTIALS####################
 def create_credentials(username, cred_app, cred_username, cred_password):
@@ -28,28 +28,31 @@ def create_credentials(username, cred_app, cred_username, cred_password):
       Function to create a new user credentials
       '''
       new_credential = Credentials(username, cred_app, cred_username, cred_password)
-      return new_credential
+      return new_credential#end create_cred
 
 def save_credentials(credential):
       '''
       Function to save credential
       '''
-      credential.save_credential()
+      credential.save_credential()#end save_cred
 
 def delete_crededential(credential):
       '''
       Function to delete a credential
       '''
-      credential.delete_credential()
+      credential.delete_credential()#end delete cred
 
 def find_credential(username):
     '''
     Function that finds a credential by username and returns the user credential
     '''
-    return Credentials.find_by_username(username)
+    return Credentials.find_by_username(username)#end find cred
 
 def display_credentials():
     '''
     Function that returns all the saved credentials
     '''
-    return Credentials.display_credentials()
+    return Credentials.display_credentials()#end display cred
+
+##########GENERIC################################
+
