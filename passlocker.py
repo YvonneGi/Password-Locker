2ae6b2c27c707b3a0ab69f7b914cbba9b20b6a6b
YvonneGi <giryvonne@gmail.com>
2019-09-06 11:51:25 +0200

    create a function to save a user account

--- passlocker.py
+++ passlocker.py
@@ -5,7 +5,13 @@ from credentials import Credentials
 #######USER########
 def create_user(username,password):
     '''
-    Function to create a new user acoount
+    Function to create a new user account
     '''
     new_user = User(username,password)
     return new_user
+
+def save_user(user):
+    '''
+    Function to save user
+    '''
+    user.save_user()
