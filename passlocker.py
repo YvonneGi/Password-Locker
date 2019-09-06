74c768927dc190cff5a996f76fe568c785c5f715
YvonneGi <giryvonne@gmail.com>
2019-09-06 14:18:52 +0200

    add main method

--- passlocker.py
+++ passlocker.py
@@ -2,57 +2,91 @@
 from user import User
 from credentials import Credentials
 
-#######USER########
+   #######USER########
+
 def create_user(username, password):
-      '''
-      Function to create a new user account
-      '''
-      new_user = User(username, password)
-      return new_user#end create_user
+        '''
+        Function to create a new user account
+        '''
+        new_user = User(username, password)
+        return new_user  # end create_user
 
 def save_user(user):
-      '''
-      Function to save user
-      '''
-      user.save_user()#end save_user
+        '''
+        Function to save user
+        '''
+        user.save_user()  # end save_user
 
 def login_user(username, password):
-      '''
-      Function that finds a user by username/password and returns the user account
-      '''
-      return User.login_by_userpass(username, password)#end login
+        '''
+        Function that finds a user by username/password and returns the user account
+        '''
+        return User.login_by_userpass(username, password)  # end login
 
-#######CREDENTIALS####################
+    #######CREDENTIALS####################
+
 def create_credentials(username, cred_app, cred_username, cred_password):
-      '''
-      Function to create a new user credentials
-      '''
-      new_credential = Credentials(username, cred_app, cred_username, cred_password)
-      return new_credential#end create_cred
+        '''
+        Function to create a new user credentials
+        '''
+        new_credential = Credentials(
+            username, cred_app, cred_username, cred_password)
+        return new_credential  # end create_cred
 
 def save_credentials(credential):
-      '''
-      Function to save credential
-      '''
-      credential.save_credential()#end save_cred
+        '''
+        Function to save credential
+        '''
+        credential.save_credential()  # end save_cred
 
 def delete_crededential(credential):
-      '''
-      Function to delete a credential
-      '''
-      credential.delete_credential()#end delete cred
+        '''
+        Function to delete a credential
+        '''
+        credential.delete_credential()  # end delete cred
 
 def find_credential(username):
-    '''
-    Function that finds a credential by username and returns the user credential
-    '''
-    return Credentials.find_by_username(username)#end find cred
+        '''
+        Function that finds a credential by username and returns the user credential
+        '''
+        return Credentials.find_by_username(username)  # end find cred
 
 def display_credentials():
-    '''
-    Function that returns all the saved credentials
-    '''
-    return Credentials.display_credentials()#end display cred
+        '''
+        Function that returns all the saved credentials
+        '''
+        return Credentials.display_credentials()  # end display cred
 
-##########GENERIC################################
+    ##########GENERIC################################
 
+def menu():
+        '''
+        Function to display the Main Menu
+        '''
+        print("Hello,Welcome to your accounts manager!")
+        print('\n')
+        print('\n')
+        print("             What would you like to do?")
+        print('\n')
+        print("             1. Sign Up")
+        print('\n')
+        print("             2. Login")
+        print('\n')
+        print("             3. Exit")  # end menu
+
+def log_menu():
+        '''
+        Function to display the menu for a logged in user
+        '''
+
+        print("             a. Add Credential")
+        print("             b. Search Credential")
+        print("             c. Display all Credentials")
+        print("             d. Delete Credential")
+        print("             e. Logout")  # end log_menu
+
+# if __name__ == '__main__':
+
+#   main()
+
+##################################################################################
