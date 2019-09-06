c7376a9614cfba1e224d85954c34f8c7792aa205
YvonneGi <giryvonne@gmail.com>
2019-09-06 11:49:38 +0200

    create a function to create a user account

--- passlocker.py
+++ passlocker.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3.6
 from user import User
 from credentials import Credentials
+
+#######USER########
 def create_user(username,password):
     '''
     Function to create a new user acoount
