c1f60d873cd596283958f4394a8f32e7390879f0
YvonneGi <giryvonne@gmail.com>
2019-09-07 14:16:31 +0200

    add copy function to copy credentials to clipboard

--- credentials.py
+++ credentials.py
@@ -31,6 +31,13 @@ class Credentials:
         '''
 
         Credentials.cred_list.remove(self)
+    
+	# def generate_password(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+	# 	'''
+	# 	Function to generate an 8 character password for a credential
+	# 	'''
+	# 	gen_pass=''.join(random.choice(char) for _ in range(size))
+	# 	return gen_pass
 
   #####search for a credential in stored credentials#####
     @classmethod
